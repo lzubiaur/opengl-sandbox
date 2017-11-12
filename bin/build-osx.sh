14dd97a513c0d5913627898472f7e3ac934d304f
lzubiaur <zubiaur.laurent@gmail.com>
2017-11-12 19:29:15 +0100

    Initial commit

---
+++ bin/build-osx.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# generate GL3W if needed
+if ! [ -f "lib/gl3w/src/gl3w.c" ]; then
+  pushd lib/gl3w
+  ./gl3w_gen.py
+  popd
+fi
+
+build='Release'
+if [ "$1" == "debug" ]; then
+  build='Debug'
+fi
+
+if ! [ -d "build/osx" ]; then
+  mkdir -p "build/osx"
+fi
+cd build/osx
+
+# C and C++ compiler
+export CC="clang"
+export CXX="clang++"
+
+msg=" --------------------- Build Succeeded ---------------------"
+
+# Generate build system using the "XCode" or "Unix Makefiles" generator.
+# cmake -G "Xcode" \
+cmake -G "Unix Makefiles" \
+    -DBUILD_OSX=TRUE \
+    -DCMAKE_BUILD_TYPE=$build \
+    ../..
+
+# Build and install the project using the Release config
+cmake --build . --target rt --config $build && echo $msg

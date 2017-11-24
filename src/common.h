aa165bb12271469240e62d6b804de6f9acc1905e
Laurent Zubiaur <zubiaur.laurent@gmail.com>
2017-11-24 17:44:23 +0100

    Add common header

---
+++ src/common.h
@@ -0,0 +1,24 @@
+#ifndef ENGINE_COMMON_H
+#define ENGINE_COMMON_H
+
+#include "spdlog/spdlog.h"
+
+namespace Engine {
+
+#define ENGINE_DEBUG
+
+#ifdef ENGINE_DEBUG
+#define LOG_DEBUG_ON
+#endif // ENGINE_DEBUG
+
+#ifdef LOG_DEBUG_ON
+#define LOG_DEBUG(...) spdlog::get("console")->debug(__VA_ARGS__)
+#define LOG_DEBUG_IF(flag, ...) spdlog::get("console")->debug_if(flag, __VA_ARGS__)
+#else
+#define LOG_DEBUG(...)
+#define LOG_DEBUG_IF(flag, ...)
+#endif
+
+} /* Engine */
+
+#endif // ENGINE_COMMON_H

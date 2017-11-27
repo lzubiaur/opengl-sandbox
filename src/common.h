b83a554e9af1588ca4a0978730ac7daa62ecb3da
Laurent Zubiaur <zubiaur.laurent@gmail.com>
2017-11-27 19:13:02 +0100

    Add logging

--- src/common.h
+++ src/common.h
@@ -11,10 +11,19 @@ namespace Engine {
 #define ENGINE_GL_MINOR 3
 
 #ifdef ENGINE_DEBUG
+#define LOG_ON
 #define LOG_DEBUG_ON
 #endif // ENGINE_DEBUG
 
-#ifdef LOG_DEBUG_ON
+#ifdef LOG_ON
+#define LOG_ERROR(...) spdlog::get("console")->error(__VA_ARGS__)
+#define LOG_ERROR_IF(flag, ...) spdlog::get("console")->error_if(flag, __VA_ARGS__)
+#else
+#define LOG_ERROR(...)
+#define LOG_ERROR_IF(flag, ...)
+#endif
+
+#if defined LOG_ON && defined LOG_DEBUG_ON
 #define LOG_DEBUG(...) spdlog::get("console")->debug(__VA_ARGS__)
 #define LOG_DEBUG_IF(flag, ...) spdlog::get("console")->debug_if(flag, __VA_ARGS__)
 #else
